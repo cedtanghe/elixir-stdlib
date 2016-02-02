47934b42f9532171089f9d8edbbd4cb6e9dadde9
cedtanghe <ced.tanghe@gmail.com>
2016-02-02 11:12:39 +0100

    Add Auth facade

---
+++ src/Facade/Auth.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Elixir\STDLib\Facade;
+
+use Elixir\STDLib\FacadeTrait;
+
+/**
+ * @author Cédric Tanghe <ced.tanghe@gmail.com>
+ */
+class Auth 
+{
+    use FacadeTrait;
+    
+    /**
+     * {@inheritdoc}
+     */
+    protected static function getFacadeAccessor() 
+    {
+        return 'Elixir\Security\Auth\AuthManager';
+    }
+}
